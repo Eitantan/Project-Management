25ebe6e33f12a78db62d365f2fbb23b7cc7f60fa
Eitantan <56890550+Eitantan@users.noreply.github.com>
2020-10-09 08:23:49 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,4 +5,5 @@ const pm = {};
 pm.addItem = function(item){projects.push(item)}
 pm.read = function(type){if(type=="return"({return projects}else if(type=="console"){console.log(projects}else{throw new Error("No type specified.")}}
 pm.delete = function(index){delete projects[index]}
-pm.readFileForArray = function(filename, encoding="utf-8"){fs.readFile(filename, encoding, (err,data){})}
+// Don't know how to do this. Maybe next minor version.
+// pm.readFileForArray = function(filename, encoding="utf-8"){fs.readFile(filename + ".js", encoding, (err,data){if (err){ throw err } else return null})}
