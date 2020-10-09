c5aba6b3d557f14a85d9943bfaac8d6676fa1e17
Eitantan <56890550+Eitantan@users.noreply.github.com>
2020-10-09 08:36:53 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
-const fs = require("fs");
-const projects = [];
+const fs = require("fs"), projects = [];
 
 const pm = {};
 pm.addItem = function(item){projects.push(item)}
@@ -7,3 +6,4 @@ pm.read = function(type){if(type=="return"({return projects}else if(type=="conso
 pm.delete = function(index){delete projects[index]}
 // Don't know how to do this. Maybe next minor version.
 // pm.readFileForArray = function(filename, encoding="utf-8"){fs.readFile(filename + ".js", encoding, (err,data){if (err){ throw err } else return null})}
+pm.createPublicList = function(){}
